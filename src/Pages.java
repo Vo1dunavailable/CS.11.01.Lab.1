9ec7a36a61f58d305aa12bd79071399b6de0a966
vo1d <voidunavailable1@gmail.com>
2024-08-28 09:22:56 +0800

    Completed lab

--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter your age: ");
+    int age = scanner.nextInt();
+    int pagesToRead = 100 - age;
+    System.out.println(age + "-year olds should read at least " + pagesToRead + " pages before giving up on a book.");
     }
 }
